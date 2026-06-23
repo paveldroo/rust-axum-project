101f5b3977a6b94d18f26c17632eaae41013c6a2
Pavel Druzhinin <paveldroo@gmail.com>
2026-06-24 00:07:19 +0300

    hello world route done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{Router, response::Html, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route(
+        "/hello",
+        get(|| async { Html("<strong>Hello world!</strong>") }),
+    );
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    println!("--> LISTENING on 127.0.0.1:8080\n");
+    axum::serve(listener, app).await.unwrap()
 }
