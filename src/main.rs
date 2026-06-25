5e5b7451f71f1f40efad78553f38ec88e6ac31c9
Pavel Druzhinin <paveldroo@gmail.com>
2026-06-25 13:42:36 +0300

    add server error to client error converter

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,20 @@ pub use self::error::{Error, Result};
 use std::net::SocketAddr;
 
 use axum::{
-    Router,
+    Json, Router,
     extract::{Path, Query},
+    http::status,
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
 };
 
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx;
 mod error;
@@ -54,7 +57,29 @@ async fn main() -> Result<()> {
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
     println!();
-    res
+
+    let uuid = Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!("  ->> server log line -- {uuid} -- Error: {service_error:?}");
+
+    error_response.unwrap_or(res)
 }
 
 fn routes_hello() -> Router {
